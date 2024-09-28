dcfbd5ab22bcfd646db285de18fa88c98e1063c0
GiovanniPeirone <mgpeirone25@gmail.com>
2024-09-28 00:54:09 -0300

    asd

--- index.js
+++ index.js
@@ -6,42 +6,13 @@ app.use(express.json());
 app.use(express.static('UI'));
 
 
-let Tecla = 'Null';
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/UI/PC.html')
 });
 
-app.get('/api/info', (req, res) => {
-  res.json({ 
-    mensaje: Tecla
- });
+app.get('api/data', (req, res) => {
+  let mensaje = { tecla : "null"}
 });
-
-app.put('/api/info', (req, res) => {
-  // Aquí debes definir qué hacer con los datos que recibes
-  const newValue = req.body.value;
-  // Actualiza la información como desees
-  data.value = newValue; // Suponiendo que `data` es tu objeto de datos
-  res.json(data); // Responde con el objeto actualizado
-});
-
-app.post('/api/valor', (req, res) => {
-  const nuevoValor = req.body.valor;
-  
-  const mensaje = req.body.mensaje;
-    console.log('Mensaje recibido:', mensaje);
-    // Respuesta de ejemplo
-
-
-  if (nuevoValor) {
-    valorAPI = nuevoValor;
-    res.json({ mensaje: 'Valor actualizado correctamente', nuevoValor });
-  } else {
-    res.status(400).json({ mensaje: 'Valor no proporcionado' });
-  }
-});
-
 
 app.listen(port, () => {
   console.log(`API escuchando en http://localhost:${port}`);
