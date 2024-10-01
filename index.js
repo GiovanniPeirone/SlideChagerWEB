c17e911e98299b0ee2e563665470cfbff0d6c82e
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-09-30 22:04:57 -0300

    asdf

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.static('UI'));
 
+/*
+null : Nada
+true : derecha (right)
+false: isquierda (left)
+*/
+let key = null;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/UI/PC.html')
+  res.sendFile(path.join(__dirname, 'UI', 'mobile.html'));
 });
 
-app.get('api/data', (req, res) => {
-  let mensaje = { tecla : "null"}
+app.get('api/key', (req, res) => {
+  res.json(key)
 });
+
+
+app.post('api/change_data', (req, res) => {
+  const newKey = req.body.key;
+  key = newKey;
+  console.log(`The new vasle id ${key}`);
+  res.json( { key : key } );
+});
+
 
 app.listen(port, () => {
   console.log(`API escuchando en http://localhost:${port}`);
